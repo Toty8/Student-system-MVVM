41341653b75da01b08e9739402f947c4d1d01d81
Toty8 <80042351+Toty8@users.noreply.github.com>
2025-03-27 16:00:52 +0200

    Add UserHelper class

---
+++ WelcomeExtended/Helpers/UserHelper.cs
@@ -0,0 +1,12 @@
+﻿using Welcome.Model;
+
+namespace WelcomeExtended.Helpers
+{
+    public static class UserHelper
+    {
+        public static void ToUserString(this User user) 
+        { 
+
+        }
+    }
+}
