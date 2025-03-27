60ac4d9778aaffd763c745149478667bf177f075
Toty8 <80042351+Toty8@users.noreply.github.com>
2025-03-27 15:19:22 +0200

    Add Validation methods with foreach, lambda and linq

--- WelcomeExtended/Data/UserData.cs
+++ WelcomeExtended/Data/UserData.cs
@@ -4,24 +4,50 @@ namespace WelcomeExtended.Data
 {
     public class UserData
     {
-        private List<User> users;
+        private List<User> _users;
         private int _nextId;
 
         public UserData()
         {
-            users = new List<User>();
+            _users = new List<User>();
             _nextId = 0;
         }
 
         public void AddUser(User user)
         {
             user.Id = _nextId++;
-            users.Add(user);
+            _users.Add(user);
         }
 
         public void DeleteUser(User user)
         {
-            users.Remove(user);
+            _users.Remove(user);
+        }
+
+        public bool ValidateUser(string name, string password)
+        {
+            foreach (var user in _users)
+            {
+                if (user.Name == name && user.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool ValidateLambda(string name, string password)
+        {
+            return _users.Where(x => x.Name == name && x.Password == password).Any();
+        }
+
+        public bool ValidateUserLinq(string name, string password)
+        {
+            var ret = from user in _users
+                      where user.Name == name && user.Password == password
+                      select user.Id;
+
+            return ret != null ? true : false;
         }
     }
 }
