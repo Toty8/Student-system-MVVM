cf49b1615a61ddfd2a67b2571c642e9ca96301a4
Toty8 <80042351+Toty8@users.noreply.github.com>
2025-03-30 19:47:01 +0300

    Configure Sqlite database

--- DataLayer/Database/DatabaseContext.cs
+++ DataLayer/Database/DatabaseContext.cs
@@ -6,7 +6,10 @@ namespace DataLayer.Database
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
+            string solutionFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string databaseFile = "Welcome.db";
+            string databasePath = Path.Combine(solutionFolder, databaseFile);
+            optionsBuilder.UseSqlite($"Data source={databasePath}");
         }
     }
 }
