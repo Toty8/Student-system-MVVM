a5c6a53c34b24e8d94b40b234e031f6c02dd7e2c
Toty8 <80042351+Toty8@users.noreply.github.com>
2025-04-14 19:37:31 +0300

    Create UI

--- DataLayer/Database/DatabaseContext.cs
+++ DataLayer/Database/DatabaseContext.cs
@@ -9,7 +9,7 @@ namespace DataLayer.Database
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string solutionFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string databaseFile = "Welcome.db";
+            string databaseFile = "Welcome1.db";
             string databasePath = Path.Combine(solutionFolder, databaseFile);
             optionsBuilder.UseSqlite($"Data source={databasePath}");
         }
@@ -19,18 +19,30 @@ namespace DataLayer.Database
 
             var user = new DatabaseUser("databaseUser", "password", "email", UserRolesEnum.ADMIN);
 
+            user.Id = 1;
+            user.FacultyNumber = "userFacultyNumber";
             user.Expires = DateTime.UtcNow.AddYears(10);
-
-            var user2 = new DatabaseUser("databaseUser2", "password2", "email2", UserRolesEnum.PROFESSOR);
-
-            user2.Expires = DateTime.UtcNow.AddMonths(4);
-
-            var user3 = new DatabaseUser("databaseUser3", "password3", "email3", UserRolesEnum.INSPECTOR);
-
-            user3.Expires = DateTime.UtcNow.AddDays(74);
 
             modelBuilder.Entity<DatabaseUser>()
                 .HasData(user);
+
+            var user2 = new DatabaseUser("databaseUser2", "password2", "email2", UserRolesEnum.PROFESSOR);
+
+            user2.Id = 2;
+            user2.FacultyNumber = "user2FacultyNumber";
+            user2.Expires = DateTime.UtcNow.AddMonths(4);
+
+            modelBuilder.Entity<DatabaseUser>()
+                .HasData(user2);
+
+            var user3 = new DatabaseUser("databaseUser3", "password3", "email3", UserRolesEnum.INSPECTOR);
+
+            user3.Id = 3;
+            user3.FacultyNumber = "user3FacultyNumber";
+            user3.Expires = DateTime.UtcNow.AddDays(74);
+
+            modelBuilder.Entity<DatabaseUser>()
+                .HasData(user3);
         }
 
         public DbSet<DatabaseUser> Users { get; set; }
--- DataLayer/Program.cs
+++ DataLayer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DataLayer.Database;
 using DataLayer.Model;
+using Welcome.Model;
 using Welcome.Others;
 
 namespace DataLayer
@@ -11,10 +12,27 @@ namespace DataLayer
             using (var context = new DatabaseContext())
             {
                 context.Database.EnsureCreated();
-                context.Add<DatabaseUser>(new DatabaseUser("user", "password", "email", UserRolesEnum.STUDENT));
+                //DatabaseUser inputUser = new DatabaseUser("user", "password", "email", UserRolesEnum.STUDENT);
+                //context.Add<DatabaseUser>(inputUser);
+                //inputUser.Id = 5;
+                //inputUser.FacultyNumber = "inputUserFacultyNumber";
+                //inputUser.Expires = DateTime.UtcNow.AddYears(7);
                 context.SaveChanges();
                 var users = context.Users.ToList();
-                Console.ReadKey();
+
+
+                Console.WriteLine("Enter username:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter password:");
+                string password = Console.ReadLine();
+
+                bool exists = (from user in users
+                               where user.Name == name && user.Password == password
+                               select user).Any();
+
+                string result = exists ? "Valid User!" : "Invalid User!";
+
+                Console.WriteLine(result);
             }
         }
     }
